c91cc7205bfbd9e307fbb5330abba17663561d6e
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2023-10-14 21:39:59 -0400

    feat: config database

---
+++ src/ormconfig.ts
@@ -0,0 +1,15 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: '123',
+  database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts, .js}'],
+  // synchronize every time read and create tables (avoid in production)
+  synchronize: true,
+};
+
+export default config;
